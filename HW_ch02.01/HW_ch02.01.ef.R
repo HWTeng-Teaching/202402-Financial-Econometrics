25f9fc84d2ac1578f559480d0a592c7c238044be
游政諺 <121661634+adni7413@users.noreply.github.com>
2024-03-07 21:44:04 +0800

    Add files via upload

---
+++ HW_ch02.01/HW_ch02.01.ef.R
@@ -0,0 +1,39 @@
+#2.1 e
+
+x = c(-1,0,1,2,3)
+y = c(1,0,3,2,4)
+estimate_y = c(0.4,1.2,2,2.8,3.6)
+
+plot(x, y,
+     xlab="x", 
+     ylab="y",
+     pch = 16, 
+     col = 1
+     )
+points(x,estimate_y, 
+       pch=16,                
+       col="blue")  
+lines(x, estimate_y, col = "blue")
+
+
+#2.1 f
+
+x = c(-1,0,1,2,3)
+y = c(1,0,3,2,4)
+estimate_y = c(0.4,1.2,2,2.8,3.6)
+
+plot(x, y,
+     xlab="x", 
+     ylab="y",
+     pch = 16, 
+     col = 1
+)
+points(x,estimate_y, 
+       pch=16,                
+       col="blue")  
+lines(x, estimate_y, col = "blue")
+
+
+points(1,2, 
+       pch=16,                
+       col="red")  
