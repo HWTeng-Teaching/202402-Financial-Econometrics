c62cd724cdb7268e5105463e9b86ea0af4d63e56
hsin456 <145751254+hsin456@users.noreply.github.com>
2024-03-08 14:15:58 +0800

    Update HW_ch02.25.c,d.R

--- HW_ch02.25/HW_ch02.25.c,d.R
+++ HW_ch02.25/HW_ch02.25.c,d.R
@@ -1,10 +1,9 @@
-# This homework is written by 312707001 葉宗翰
-
-#Download the required data and set up variables and linear models.
+# This homework is written by 312707006 黃馨霈
 
 library(POE5Rdata)
 data("cex5_small")
 
+#2.25(c)
 cex5_small$lnfoodaway <- log(cex5_small$foodaway)
 cex5_small$lnfoodaway[is.infinite(cex5_small$lnfoodaway)] <- NA
 
@@ -12,5 +11,6 @@ hist(cex5_small$lnfoodaway)
 
 summary(cex5_small$lnfoodaway)
 
+#2.25(d)
 lm_Income_lnFoodaway <- lm(lnfoodaway ~ income, data = cex5_small)
 summary(lm_Income_lnFoodaway)
