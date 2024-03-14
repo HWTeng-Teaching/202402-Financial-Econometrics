d7bf0e27961be39a79b51205070e85ba6f373bfc
viviand6080 <162035476+viviand6080@users.noreply.github.com>
2024-03-14 22:16:34 +0800

    Update HW_ch02.11.e,f.R

--- HW_ch02.11/HW_ch02.11.e,f.R
+++ HW_ch02.11/HW_ch02.11.e,f.R
@@ -1,11 +1,9 @@
 #2.11 (e)
 x1 <- 20
-y_hat1 <- exp(3.14 + 0.007*x1)
 slope1 <- 0.007*exp(3.14 + 0.007*x1)
 print(slope1)
 
 #2.11 (f)
 x2 <- 30
-y_hat2 <- exp(3.14 + 0.007*x2)
 slope2 <- 0.007*exp(3.14 + 0.007*x2)
 print(slope2)
