c93236c46f51d14f4ba41be48e12c143ac478bb5
Galong123 <161788384+Galong123@users.noreply.github.com>
2024-03-27 17:39:53 +0800

    Create HW_ch04.25.d

---
+++ HW_ch04.25/HW_ch04.25.d
@@ -0,0 +1,11 @@
+
+## This homework is written by 張家榮 (312707049)
+
+## ch04.25(d)
+
+**Question** \
+Construct histograms of the least squares residuals from each of the models in (a)~(c) and obtain the Jarque-Bera statistics. Based on your observations of the residuals to be compatible with an assumption of normality ?
+
+## *Ans* 
+
+"C:\Users\neal9\OneDrive\桌面\log_linear_residual.png"
