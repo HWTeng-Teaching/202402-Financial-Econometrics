7bdfa939e6b8984971795a791bb28a49fcdd6ada
wuriwei <162611288+wuriwei@users.noreply.github.com>
2024-03-28 23:53:29 +0800

    Update HW_ch04.25.b.R

--- HW_ch04.25/HW_ch04.25.b.R
+++ HW_ch04.25/HW_ch04.25.b.R
@@ -11,3 +11,8 @@ elasticity <- slope * (mean(collegetown$sqft) / mean(collegetown$price))
 
 print(paste("斜率：", slope))
 print(paste("彈性：", elasticity))
+
+price_mean <- mean(collegetown$price)
+sqft_mean <- mean(collegetown$sqft)
+print(paste("price平均數", price_mean))
+print(paste("sqft平均數", sqft_mean))
