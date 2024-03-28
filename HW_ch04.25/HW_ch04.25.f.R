746633cb228f1ee26194e09df78e4219662322a3
cloryYH <101499792+cloryYH@users.noreply.github.com>
2024-03-28 14:50:01 +0800

    Update HW_ch04.25.f.R

--- HW_ch04.25/HW_ch04.25.f.R
+++ HW_ch04.25/HW_ch04.25.f.R
@@ -3,23 +3,23 @@ data("collegetown")
 
 sqft_f=2700/100
 
-model1=lm(price~log(sqft),data=collegetown)
+model1=lm(log(price)~sqft,data=collegetown)
 #summary(model1)
-b1_a=coef(model1)[[1]]
-b2_a=coef(model1)[[2]]
-y1=b1_a+b2_a*log(sqft_f)
+b1=coef(model1)[[1]]
+b2=coef(model1)[[2]]
+y1=exp(b1+b2*sqft_f)
 print(y1*1000)
 
 model2=lm(log(price)~log(sqft),data=collegetown)
 #summary(model2)
-b1_b=coef(model2)[[1]]
-b2_b=coef(model2)[[2]]
-y2=exp(b1_b+b2_b*log(sqft_f))
+a1=coef(model2)[[1]]
+a2=coef(model2)[[2]]
+y2=exp(a1+a2*log(sqft_f))
 print(y2*1000)
 
 model3=lm(price~sqft,data=collegetown)
 #summary(model3)
-b1_c=coef(model3)[[1]]
-b2_c=coef(model3)[[2]]
-y3=b1_c+b2_c*sqft_f
+d1=coef(model3)[[1]]
+d2=coef(model3)[[2]]
+y3=d1+d2*sqft_f
 print(y3*1000)
