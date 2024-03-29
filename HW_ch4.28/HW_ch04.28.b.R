cb34c6a39cb1e3c137f9f4be55dcd8cf70bd8959
Louis0506 <145032062+52256223@users.noreply.github.com>
2024-03-29 09:42:11 +0800

    Update HW_ch04.28.b.R

--- HW_ch4.28/HW_ch04.28.b.R
+++ HW_ch4.28/HW_ch04.28.b.R
@@ -1,8 +1,9 @@
-#R square
-R_square <- matrix(c("linear-linear","linear-log","linear-quadratic","log-linear"
-                     ,result1$r.squared,result2$r.squared,result3$r.squared,result4$r.squared) , ncol = 2) 
-R_square <- t(R_square)
-R_square
+#log-linear model
+logline_model = lm(I(log(northampton)) ~ time , data = data)
+result4 = summary(logline_model)
+coe4 = c(logline_model$coefficients)
+plot(logline_model$residuals , main = "log-linear residuals" , xlab = "Time" , ylab = "Residuals") #residuals
 plot(data$time , data$northampton , xlab = "Time" , ylab = "Yield" ,
-     main = expression(widehat(Yield) == 0.7737+0.0005*Time^2)) #fitted equations
-curve(coe3[1]+ coe3[2]*x^2 , lwd = 3 , col = "red", add = T)
+     main = expression(widehat(log(Yield)) == -0.3639+0.0186*Time)) #fitted equations
+curve(exp(coe4[1]+ coe4[2]*x) , lwd = 3 , col = "red", add = T)
+
