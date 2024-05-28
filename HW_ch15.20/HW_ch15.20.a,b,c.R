64c349a9cff8125f4a9589cb1e8cd4a6e35d28f9
張永濬 <145115138+changyungchun@users.noreply.github.com>
2024-05-28 18:03:55 +0800

    Update HW_ch15.20.a,b,c.R

--- HW_ch15.20/HW_ch15.20.a,b,c.R
+++ HW_ch15.20/HW_ch15.20.a,b,c.R
@@ -1,1 +1,28 @@
+```{r echo=TRUE, message=FALSE, warning=FALSE}
+rm(list=ls())
+library(POE5Rdata)
+library(plm)
+library(nlme)
 
+# a
+data("star")
+star.pd <- pdata.frame(star, index = c("schid"))
+# Estimate a pooled OLS model
+model.pooling <- plm(readscore ~ small + aide + tchexper + boy + white_asian + freelunch,data = star.pd, model = "pooling")
+summary(model.pooling)
+a= lm(readscore ~ small + aide + tchexper + boy + white_asian + freelunch,data = star)
+summary(a)
+
+# b
+star.pd <- pdata.frame(star, index = c("schid")) 
+model.within <- plm(readscore ~ small + aide + tchexper + boy + white_asian + freelunch, data = star.pd, model = "within")
+summary(model.within)
+
+# c
+pFtest(model.within, model.pooling)
+df1 = 78
+df2 = 5681
+qf(0.95, df1, df2)
+
+
+```
