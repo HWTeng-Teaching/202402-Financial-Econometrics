c20372eb9bfae316885846efa6e54fc644702ab9
ivywu0807 <62731023+ivywu0807@users.noreply.github.com>
2024-06-03 11:32:49 +0800

    Create HW_ch15.20.f

---
+++ HW_ch15.20/HW_ch15.20.f
@@ -0,0 +1,50 @@
+## Ch15.20 (f)
+
+This homework is written by 吳悅寧 (312707056)
+
+**Question**\
+Create school-averages of the variables and carry out the Mundlak test for correlation between them and the unobserved heterogeneity.
+**Answer**
+
+-   Sample code
+``` r
+load("/Users/ivywu/Downloads/poe5rdata/star.rdata")
+
+# Rename to stuid because R does not like the variable name id
+star$stuid <- star$id
+install.packages("plm")
+library(plm)
+install.packages("car")
+library(car)
+
+# Get average of variables by teacher
+teacher.mean <- aggregate(cbind(boy, white_asian, freelunch) ~ tchid, star, mean) # Rename columns to indicate they are averages
+names(teacher.mean) <- paste(names(teacher.mean), c("", "m", "m", "m"), sep = "") # Merge averages with original panel data
+star.m <- merge(star, teacher.mean, on = "tchid")
+# Estimate random effects with averaged variables
+plm.random.m <- plm(readscore ~ small + aide + tchexper + tchmasters + boy +
+                      white_asian + freelunch + boym + white_asianm + freelunchm,
+                    data = star.m,
+                    index = c("tchid", "stuid"),
+                    model = "random",
+                    random.method = "walhus")
+# Test that average variables are jointly different from 0
+
+##
+linearHypothesis(plm.random.m, c("boym = 0", "white_asianm = 0", "freelunchm = 0"))
+Hypothesis:
+boym = 0
+white_asianm = 0
+freelunchm = 0
+
+Model 1: restricted model
+Model 2: readscore ~ small + aide + tchexper + tchmasters + boy + white_asian + 
+    freelunch + boym + white_asianm + freelunchm
+
+  Res.Df Df  Chisq Pr(>Chisq)  
+1   5758                       
+2   5755  3 7.2087    0.06554 .
+##
+``` 
+
+Testing the joint significance of these variables, we obtain a Wald test of 7.2087, The critical value of 5% = 7.815, we cannot reject the null hypothesis that the random effects are uncorrelated with the regressors.
