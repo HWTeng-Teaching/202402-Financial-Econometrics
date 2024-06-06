4be0fd761cf9afb12170b06fe2018eb77d41a376
Howardwuwu <161539032+Howardwuwu@users.noreply.github.com>
2024-06-06 23:58:10 +0800

    Create HW_ch15.19.c

---
+++ HW_chap15.19/HW_ch15.19.c
@@ -0,0 +1,5 @@
+The F-test statistic is 16.70. The (  0.95,78,5681), F = 1.2798 so that we reject the null hypothesis that 
+there are no significant differences between schools. If the school indicator variables are 
+uncorrelated with the included explanatory variables then their inclusion, or exclusion, should 
+have little effect on the regression estimates.
+  
